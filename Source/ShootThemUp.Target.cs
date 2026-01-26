03cf2e149c98ac775acbda9c1afc9e7762cf0344
Timur Shkabrov <tshkabrov@gmail.com>
2026-01-26 18:43:10 +0500

    add rifle AO to AnimBP

--- Source/ShootThemUp.Target.cs
+++ Source/ShootThemUp.Target.cs
@@ -8,7 +8,7 @@ public class ShootThemUpTarget : TargetRules
 	public ShootThemUpTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "ShootThemUp" } );
 	}
--- Source/ShootThemUpEditor.Target.cs
+++ Source/ShootThemUpEditor.Target.cs
@@ -8,7 +8,7 @@ public class ShootThemUpEditorTarget : TargetRules
 	public ShootThemUpEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "ShootThemUp" } );
 	}
